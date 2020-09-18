64fb02678bf9920e7b422a34c965156ca81887ac
strlla <estrellaportocarrero@gmail.com>
2020-09-18 18:49:19 -0300

    update item

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,18 +6,16 @@ import ItemLoader from '../Loaders/ItemLoader'
 import './Item.css'
 
 const Item = ({image, title, price, description}) => {
-    return(
-        <motion.div whileTap={{scale: 0.95}} className="card">
-            <div className="image-container">
-                {image ? <img src={image}/> : <ItemLoader height="inherit" width="100%" borderRadius="inherit"/>}
-            </div>
-            <div className="info-container">
-                {title ? <h1 className="info-title">{title}</h1> : <TextLoader className="info-title" width="50%" height="20px"/>}
-                {description && <h3 className="info-description">{description}</h3>}
-                {price ? <h2 className="info-price">$ {price}</h2> : <TextLoader className="info-price" width="70%" height="15px"/>}
-                {price ? null : <TextLoader className="info-description" width="60%" height="15px"/>}
-            </div>
-        </motion.div>
-    )
+    return <motion.div whileTap={{scale: 0.95}} className="card">
+        <div className="image-container">
+            {image ? <img src={image}/> : <ItemLoader height="inherit" width="100%" borderRadius="inherit"/>}
+        </div>
+        <div className="info-container">
+            {title ? <h1 className="info-title">{title}</h1> : <TextLoader className="info-title" width="50%" height="20px"/>}
+            {description && <h3 className="info-description">{description}</h3>}
+            {price ? <h2 className="info-price">$ {price}</h2> : <TextLoader className="info-price" width="70%" height="15px"/>}
+            {price ? null : <TextLoader className="info-description" width="60%" height="15px"/>}
+        </div>
+    </motion.div>
 }
-export default Item
+export default Item;
