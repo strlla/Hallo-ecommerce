e94d99232543d57f3d5e7e5ddcbfe0f41f8a8778
strlla <estrellaportocarrero@gmail.com>
2020-09-18 18:48:50 -0300

    update categories

--- src/containers/ListCategories/ListItemCategories.js
+++ src/containers/ListCategories/ListItemCategories.js
@@ -30,24 +30,22 @@ const ListItemCategories = () => {
         });
     })
     
-    return(
-        <>
-            <section className="categories-container">
-                <svg viewBox="0 0 500 250" preserveAspectRatio="none">
-                    <linearGradient id="gradient-horizontal">
-                        <stop offset="0%" stop-color="var(--color-stop-1)" />
-                        <stop offset="100%" stop-color="var(--color-stop-3)" />
-                    </linearGradient>
-                    <path  d="M0.00,92.27 C216.83,192.92 304.30,8.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z"></path>
-                </svg>
-                <div className="title-container">
-                    <motion.h1 initial="hidden" animate="show" variants={easeEffectVariants} className="title-1">{categoryId}</motion.h1>
-                </div>
-            </section>
-            <div className="list-item">
-                <ListItem Item={Item} isLoading={loading} items={items}/>
+    return <>
+        <section className="categories-container">
+            <svg viewBox="0 0 500 250" preserveAspectRatio="none">
+                <linearGradient id="gradient-horizontal">
+                    <stop offset="0%" stop-color="var(--color-stop-1)" />
+                    <stop offset="100%" stop-color="var(--color-stop-3)" />
+                </linearGradient>
+                <path  d="M0.00,92.27 C216.83,192.92 304.30,8.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z"></path>
+            </svg>
+            <div className="title-container">
+                <motion.h1 initial="hidden" animate="show" variants={easeEffectVariants} className="title-1">{categoryId}</motion.h1>
             </div>
-        </>
-    )
+        </section>
+        <div className="list-item">
+            <ListItem Item={Item} isLoading={loading} items={items}/>
+        </div>
+    </>    
 }
 export default ListItemCategories;
